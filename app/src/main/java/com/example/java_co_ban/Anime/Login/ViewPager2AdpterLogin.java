cfa61cc1067fe49fd342b281dcc98e7435220b6e
LVanVuong <73379413+LVanVuong@users.noreply.github.com>
2021-11-25 14:59:59 +0700

    Login

---
+++ app/src/main/java/com/example/java_co_ban/Anime/Login/ViewPager2AdpterLogin.java
@@ -0,0 +1,30 @@
+//package com.example.java_co_ban.Login;
+//
+//import androidx.annotation.NonNull;
+//import androidx.fragment.app.Fragment;
+//import androidx.fragment.app.FragmentActivity;
+//import androidx.viewpager2.adapter.FragmentStateAdapter;
+//
+//public class ViewPager2AdpterLogin extends FragmentStateAdapter {
+//    public ViewPager2AdpterLogin(@NonNull FragmentActivity fragmentActivity) {
+//        super(fragmentActivity);
+//    }
+//
+//    @NonNull
+//    @Override
+//    public Fragment createFragment(int position) {
+//        switch (position){
+//            case 0:
+//                LoginActivity loginActivity = new LoginActivity();
+//                return loginActivity;
+//            case 1:
+//
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 2;
+//    }
+//}
